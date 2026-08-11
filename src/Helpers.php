84c7af8aca9eb68e9fd0e2b3cf643f492bed0943
Yousef Alkurdi <yalkerdi@gmail.com>
2026-08-11 03:33:42 +0200

    feat(game): voeg multiplayer-ondersteuning toe aan newGame
    - newGame verwerkt nu meerdere spelers (standaard 'speler 1')
    - Genereer per speler op basis van index een unieke scorekaart
    - Beginstand uitgebreid met actieveSpeler tracker

---
+++ src/Helpers.php
@@ -0,0 +1,39 @@
+<?php
+
+// Start een nieuw spel (standaard 1 speler als er niks wordt meegegeven)
+function newGame($speler = ['speler 1'])
+{
+    // Blanco scorekaart-sjabloon
+    $legeCat = [
+        'ones' => null,
+        'twos' => null,
+        'threes' => null,
+        'fours' => null,
+        'fives' => null,
+        'sixes' => null,
+        'three_kind' => null,
+        'four_kind' => null,
+        'full_house' => null,
+        'small_straight' => null,
+        'large_straight' => null,
+        'chance' => null,
+        'yahtzee' => null
+    ];
+
+    // Kent aan de index (stoelnummer) van elke speler een eigen lege scorekaart toe
+    $score = [];
+    foreach($speler as $index => $naam){
+        $score[$index] = $legeCat;
+    }
+
+    return [
+        'melding'       => "",
+        'ronde'         => 1,
+        'beurt'         => 0,
+        'dobbelstenen'  => [1, 1, 1, 1, 1],
+        'vasthouden'    => [false, false, false, false, false],
+        'actieveSpeler' => 0, // Index van de speler die aan de beurt is
+        'speler'        => $speler,
+        'score'         => $score
+    ];
+}
